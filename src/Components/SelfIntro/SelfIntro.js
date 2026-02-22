1453868e687f6640bb1dd9d9ecbf921f140f90d4
Adeel Madagavi <adeelmadagavi25@gmail.com>
2026-02-22 18:03:56 +0530

    Fixed Resume build errors

--- src/Components/SelfIntro/SelfIntro.js
+++ src/Components/SelfIntro/SelfIntro.js
@@ -2,112 +2,128 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/ami.png";
 import Tilt from "react-parallax-tilt";
+
 import {
   AiFillGithub,
   AiOutlineYoutube,
   AiFillInstagram,
 } from "react-icons/ai";
+
 import { FaLinkedinIn } from "react-icons/fa";
 
 function SelfIntro() {
   return (
-    <Container fluid className="home-about-section  container-fluid" id="about">
+    <Container fluid className="home-about-section container-fluid" id="about">
       <Container>
+
         <Row>
           <Col md={8} className="home-about-description">
+
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              LET ME <span className="purple">INTRODUCE</span> MYSELF
             </h1>
+
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Java and JavaScript. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies  &nbsp;</b> and also passionate
-                about all things in  &nbsp;<b className="purple">Webdevelopment</b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-             
-              &nbsp; like
-              
-                <b className="purple"> React.js </b>
-              
+
+              I fell in love with programming and have experience in building modern web applications.
+
+              <br /><br />
+
+              I am fluent in
+              <b className="purple"> C++, Java and JavaScript.</b>
+
+              <br /><br />
+
+              My field of interest includes
+              <b className="purple"> Web Development</b> and building modern responsive applications.
+
+              <br /><br />
+
+              I enjoy developing applications using
+              <b className="purple"> Node.js, React.js</b> and modern JavaScript frameworks.
+
             </p>
+
           </Col>
+
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img
+                src={myImg}
+                className="img-fluid"
+                alt="avatar"
+              />
             </Tilt>
           </Col>
+
         </Row>
+
+
+        {/* SOCIAL LINKS */}
+
         <Row>
           <Col md={12} className="home-about-social">
+
             <h1>FIND ME ON</h1>
-              <p>
-                Feel free to <span className="purple">connect </span>with me
-              </p>
+
+            <p>
+              Feel free to <span className="purple">connect</span> with me
+            </p>
+
             <ul className="home-about-social-links">
+
               <li className="social-icons">
-                {/* <a
-                  // href="https://github.com/adeelmadagavi"
+                <a
+                  href="https://github.com/adeelmadagavi"
                   target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  rel="noopener noreferrer"
+                  className="icon-colour home-social-icons"
                 >
                   <AiFillGithub />
-                </a> */}
+                </a>
               </li>
 
               <li className="social-icons">
-                {/* <a
-                  // href="https://www.linkedin.com/in/adeelmadagavi/"
+                <a
+                  href="https://www.linkedin.com/in/adeelmadagavi/"
                   target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  rel="noopener noreferrer"
+                  className="icon-colour home-social-icons"
                 >
                   <FaLinkedinIn />
-                </a> */}
+                </a>
               </li>
+
               <li className="social-icons">
-                {/* <a
-                  // href="https://www.instagram.com/adeelmadagavi/"
+                <a
+                  href="https://www.instagram.com/adeelmadagavi/"
                   target="_blank"
-                  rel="noreferrer"
+                  rel="noopener noreferrer"
                   className="icon-colour home-social-icons"
                 >
                   <AiFillInstagram />
-                </a> */}
+                </a>
               </li>
+
               <li className="social-icons">
-                {/* <a
-                  href="https://youtube.com/c/SijeeshMiziha"
+                <a
+                  href="https://youtube.com/"
                   target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  rel="noopener noreferrer"
+                  className="icon-colour home-social-icons"
                 >
                   <AiOutlineYoutube />
-                </a> */}
+                </a>
               </li>
+
             </ul>
+
           </Col>
         </Row>
+
       </Container>
     </Container>
   );
 }
+
 export default SelfIntro;
--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,33 +1,38 @@
-import React from 'react'
-import { Container, Row} from "react-bootstrap";
-// import Button from "react-bootstrap/Button";
+import React from "react";
+import { Container, Row } from "react-bootstrap";
 import Particle from "../Components/Particles/Particle";
- import resume from "../Assets/dd.png";
-//  import project from "../Assets/project-his.png";
-//  import pdf from "../Assets/Adeelmadagavi1.pdf"
-
-
-// import { AiOutlineDownload } from "react-icons/ai";
-
+import resume from "../Assets/dd.png";
 
 function Resume() {
-    return (
-        <Container fluid className="resume-section">
-        <Particle />
-        
-      <img src={resume} alt="error" style={{width:"70%" , marginLeft:"15%", marginBottom:"5%" }}/>
-      {/* <img src={project} alt="error" style={{width:"70%" , marginLeft:"15%" ,marginBottom:"5%" }}/> */}
+  return (
+    <Container fluid className="resume-section">
       
-        <Container>
-          <Row style={{ justifyContent: "center", position: "relative"}}>
-            {/* <Button variant="primary" href={pdf} target="_blank">
-              <AiOutlineDownload />
-              &nbsp;Download CV
-            </Button> */}
-          </Row>
-          </Container>
+      <Particle />
+
+      {/* Resume Image */}
+      <img
+        src={resume}
+        alt="Resume"
+        style={{
+          width: "70%",
+          marginLeft: "15%",
+          marginBottom: "5%",
+        }}
+      />
+
+      <Container>
+        <Row
+          style={{
+            justifyContent: "center",
+            position: "relative",
+          }}
+        >
+          {/* Download button removed safely */}
+        </Row>
+      </Container>
+
     </Container>
-    )
+  );
 }
 
-export default Resume
+export default Resume;
